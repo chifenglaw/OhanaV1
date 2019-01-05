6d86bdf42c57d6134396fb495f83b65166e7e6e7
Chifeng Law <Chifeng.Law@gmail.com>
2019-01-05 15:57:12 -0800

    Initial Commit

---
+++ OhanaV1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OhanaV1
+//
+//  Created by Chifeng Law on 1/5/19.
+//  Copyright © 2019 Chifeng Law. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
