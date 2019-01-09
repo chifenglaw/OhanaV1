357df449f06bc7f9873a9b38efd18ea1ab4ecd11
Chifeng Law <Chifeng.Law@gmail.com>
2019-01-08 17:29:34 -0800

    added detail view

--- OhanaV1/Controller/EventDetailVC.swift
+++ OhanaV1/Controller/EventDetailVC.swift
@@ -12,6 +12,12 @@ class EventDetailVC: UIViewController {
     
     var eventID = 0
     @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var eventDateLabel: UILabel!
+    @IBOutlet weak var eventTicketLabel: UILabel!
+    @IBOutlet weak var eventLocationLabel: UILabel!
+    @IBOutlet weak var eventDetailHeadingLabel: UILabel!
+    @IBOutlet weak var eventDetailLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +29,11 @@ class EventDetailVC: UIViewController {
     
     func updateUI() {
         eventImage.image = UIImage(named: "latern_festival")!
+        eventDateLabel.text = "Nov 30, 2018 - Jan 20, 2019"
+        eventLocationLabel.text = "Alameda County Fairgrounds, Pleasanton, CA"
+        eventTicketLabel.text = "https://lanternlightfestival.com/"
+        eventDetailHeadingLabel.text = "Details"
+        eventDetailLabel.text = "Join us for a spectacular evening of dazzing lights, colors, shapes, and themes. The Lantern Light festival is a celebration of Chinese culture featuring massive handmade lanterns, inspiring performances, and children's activities"
     }
     
 }
